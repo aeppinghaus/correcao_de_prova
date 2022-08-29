bf081398866ce1acc4442a736a93e8f8b929b0f8
André Eppinghaus <aeppinghaus@gmail.com>
2022-08-29 23:41:25 +0000

    Adicionando estruturas e modelos de arquivos.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+#include "services.h"
 #include <iostream>
 
 int main() {
+  Gabarito lucas;
+  Gabarito andre;
+  
+
   std::cout << "Hello World!\n";
 }
---
+++ services.h
@@ -0,0 +1,24 @@
+/**
+ *Arquivo com todas as nossas funcoes
+ **/
+#include <iostream>
+using namespace std;
+
+struct GabaritoComArray {
+  string matricula;
+  string notas[2][12];
+};
+
+struct GabaritoSemArray {
+  string matricula;
+  string q1;
+  string q2;
+  string q3;
+  string q4;
+  string q5;
+  string q6;
+  string q7;
+  string q8;
+  string q9;
+  string q10;
+};
