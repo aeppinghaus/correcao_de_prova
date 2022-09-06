af8d9ca1b135f2f0ce2ef290bfd4dd00e46a26f8
André Eppinghaus <aeppinghaus@gmail.com>
2022-09-06 00:32:51 +0000

    Leitura de arquivo; criacao de funcao para armazenar uma linha em um vetor; Pegamos a linha do vetor e colocamos em uma estrutura.

--- .ccls-cache/@home@runner@correcaodeprova/main.cpp
+++ .ccls-cache/@home@runner@correcaodeprova/main.cpp
@@ -2,10 +2,33 @@
 #include <iostream>
 
 int main() {
-  // Gabarito lucas;
-  // Gabarito andre;
 
   lerArquivo("resultado/resultado.csv");
 
   std::cout << "Hello World!\n";
+
+  // cout << variavelGlobal[1] << endl;
+
+  int linha = 0;
+  while (linha < 2) {
+    cout << variavelGlobal[linha] << endl;
+    
+    split(variavelGlobal[linha], linha);
+
+    cout << "=============" << endl;
+    cout << "Matricula:" << gabarito2[linha].matricula << endl;
+    cout << "Nome:" << gabarito2[linha].nome << endl;
+    cout << "questão 1:" << gabarito2[linha].q1 << endl;
+    cout << "questão 2:" << gabarito2[linha].q2 << endl;
+    cout << "questão 3:" << gabarito2[linha].q3 << endl;
+    cout << "questão 4:" << gabarito2[linha].q4 << endl;
+    cout << "questão 5:" << gabarito2[linha].q5 << endl;
+    cout << "questão 6:" << gabarito2[linha].q6 << endl;
+    cout << "questão 7:" << gabarito2[linha].q7 << endl;
+    cout << "questão 8:" << gabarito2[linha].q8 << endl;
+    cout << "questão 9:" << gabarito2[linha].q9 << endl;
+    cout << "questão 10:" << gabarito2[linha].q10 << endl;
+    cout << "=============" << endl;
+    linha++;
+  }
 }
--- .ccls-cache/@home@runner@correcaodeprova/services.h
+++ .ccls-cache/@home@runner@correcaodeprova/services.h
@@ -5,18 +5,24 @@
 #include <iostream>
 using namespace std;
 
+string variavelGlobal[2];
+
 // regiao de prototipos de funcao
-void split(string s);
+// separa a string a partir de um caracter
+void split(string s, int linha);
 
 // fim de regiao de prototipos de funcao
 
 struct GabaritoComArray {
   string matricula;
+  string nome;
   string notas[2][12];
 };
+GabaritoComArray gabarito1[2];
 
 struct GabaritoSemArray {
   string matricula;
+  string nome;
   string q1;
   string q2;
   string q3;
@@ -28,32 +34,94 @@ struct GabaritoSemArray {
   string q9;
   string q10;
 };
+GabaritoSemArray gabarito2[2];
 
 void lerArquivo(string nomeArquivo) {
   string line;
+  int contador = 0;
   ifstream myfile(nomeArquivo);
   if (myfile.is_open()) {
     while (getline(myfile, line)) {
-      cout << line << '\n';
-      split(line);
-    }
+      // cout << line << '\n';
+      variavelGlobal[contador] = line;
+      // split(line);
+      contador++;
+    } // fim while
     myfile.close();
   } else {
-    cout << "Nao consegue abrir o arquivo";
+    cout << "Nao conseguiu abrir o arquivo";
   }
 }
 
-// https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-void split(string s) {
-  // std::string s = "scott>=tiger>=mushroom";
-  std::string delimiter = ";";
+// string *lerArquivoCOMPONTEIRO(string nomeArquivo) {
+//   string line;
+//   string *resultado = new string[2];
+//   int contador = 0;
+//   ifstream myfile(nomeArquivo);
+//   if (myfile.is_open()) {
+//     while (getline(myfile, line)) {
+//       // cout << line << '\n';
+//       resultado[contador] = line;
+//       // split(line);
+//       contador++;
+//     } // fim while
+//     myfile.close();
+//   } else {
+//     cout << "Nao conseguiu abrir o arquivo";
+//   }
+//   return resultado;
+// }
 
+// https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+void split(string s, int linha) {
+  // std::string s = "scott>=tiger>=mushroom";
+  std::string delimiter = ",";
+  int coluna = 0;
   size_t pos = 0;
   std::string token;
   while ((pos = s.find(delimiter)) != std::string::npos) {
     token = s.substr(0, pos);
-    std::cout << token << std::endl;
+    // cout << "contador" << contador << endl;
+    // std::cout << token << std::endl;
+
+    switch (coluna) {
+    case 0:
+      gabarito2[linha].matricula = token;
+      break;
+    case 1:
+      gabarito2[linha].nome = token;
+      break;
+    case 2:
+      gabarito2[linha].q1 = token;
+      break;
+    case 3:
+      gabarito2[linha].q2 = token;
+      break;
+    case 4:
+      gabarito2[linha].q3 = token;
+      break;
+    case 5:
+      gabarito2[linha].q4 = token;
+      break;
+    case 6:
+      gabarito2[linha].q5 = token;
+      break;
+    case 7:
+      gabarito2[linha].q6 = token;
+      break;
+    case 8:
+      gabarito2[linha].q7 = token;
+      break;
+    case 9:
+      gabarito2[linha].q8 = token;
+      break;
+    case 10:
+      gabarito2[linha].q9 = token;
+      break;
+    }
     s.erase(0, pos + delimiter.length());
+    coluna++;
   }
-  std::cout << s << std::endl;
+  gabarito2[linha].q10 = s;
+  // std::cout << s << std::endl;
 }
